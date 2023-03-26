3cca9ae60be7ec7c79043f3567aacdabb4072b42
fkeyz <fkeyzuwu@gmail.com>
2023-03-26 22:05:01 +0300

    i swear this time its going to work

--- addons/dynamic_event_manager/example_scene/test.gd
+++ addons/dynamic_event_manager/example_scene/test.gd
@@ -10,12 +10,12 @@ func _input(event: InputEvent) -> void:
 
 func change_color(event: KeyClickEvent) -> void:
 	if event.click.is_action_pressed("ui_accept"):
-		var tween = create_tween().tween_property(self, "modulate", Color.RED, 1.0)
+		var tween = create_tween().tween_property(self, "modulate", Color.TRANSPARENT, 1.0)
 		await tween.finished
 
 func change_color2(event: KeyClickEvent) -> void:
 	if event.click.is_action_pressed("ui_accept"):
-		var tween = create_tween().tween_property(self, "modulate", Color.GREEN, 1.0)
+		var tween = create_tween().tween_property(self, "modulate", Color.BLACK, 1.0)
 		await tween.finished
 		Events.remove_listener(KeyClickEvent, change_color2)
 
