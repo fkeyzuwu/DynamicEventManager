088ed3468e5934ebcbac91d909f61882eb287ead
fkeyz <73611276+fkeyzuwu@users.noreply.github.com>
2025-09-15 18:11:04 +0300

    Add null/validity check for functions

--- addons/dynamic_event_manager/src/event_manager.gd
+++ addons/dynamic_event_manager/src/event_manager.gd
@@ -46,8 +46,8 @@ func invoke(event: Event) -> void:
 	
 	var listeners = _events[event_type]
 	for i in range(listeners.size() - 1, -1, -1):
-		if listeners[i].get_object() != null:
-			await listeners[i].call(event)
+		var func = listeners[i] as Callable
+		if func.is_null() or !func.is_valid():
+			listeners.remove_at(i)
 		else:
-			listeners.erase(listeners[i])
-
+			await func.call(event)
